d4ce0c89f9f5cd9db3a7aa4d7c70f095cc9f2b1a
WiseNat <nathan88wise@gmail.com>
2020-08-11 11:36:32 +0100

    Small changes

--- camera.py
+++ camera.py
@@ -5,50 +5,49 @@ import picamera
 
 
 class Camera(object):
-    thread = None  # background thread that reads frames from camera
-    frame = None  # current frame is stored here by background thread
-    last_access = 0  # time of last client access to the camera
+    thread = None  # Background thread that reads frames from camera
+    frame = None  # Current frame is stored here by background thread
+    last_access = 0  # Time of last client access to the camera
 
-    def initialize(self):
+    def initialise(self):
         if Camera.thread is None:
-            # start background frame thread
+            # Start background frame thread
             Camera.thread = threading.Thread(target=self._thread)
             Camera.thread.start()
 
-            # wait until frames start to be available
+            # Wait until frames start to be available
             while self.frame is None:
                 time.sleep(0)
 
     def get_frame(self):
         Camera.last_access = time.time()
-        self.initialize()
+        self.initialise()
         return self.frame
 
     @classmethod
     def _thread(cls):
         with picamera.PiCamera() as camera:
-            # camera setup
-            camera.resolution = (1920, 1080)
-            camera.hflip = True
-            camera.vflip = True
+            # Pi Camera setup
+            camera.resolution = (320, 240)
+            # camera.hflip = True
+            # camera.vflip = True
 
-            # let camera warm up
+            # Giving time for the camera to warm up
             camera.start_preview()
             time.sleep(2)
 
             stream = io.BytesIO()
-            for foo in camera.capture_continuous(stream, 'jpeg',
+            for foo in camera.capture_continuous(stream, "jpeg",
                                                  use_video_port=True):
-                # store frame
+                # Store current frame
                 stream.seek(0)
                 cls.frame = stream.read()
 
-                # reset stream for next frame
+                # Reset stream for next frame
                 stream.seek(0)
                 stream.truncate()
 
-                # if there hasn't been any clients asking for frames in
-                # the last 10 seconds stop the thread
+                # Stop thread if no clients have asked for a stream for 10 seconds
                 if time.time() - cls.last_access > 10:
                     break
         cls.thread = None
