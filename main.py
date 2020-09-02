3b3f25487b35b83561b73587a079e64840998232
WiseNat <nathan88wise@gmail.com>
2020-09-02 15:22:32 +0100

    Basic Support for face detection

--- main.py
+++ main.py
@@ -18,16 +18,23 @@ def generator(cam):
 
 @web_app.route("/")
 def main_page():
-    # Servo modification logic
     arg_keys = [item.lower() for item in list(request.args.keys())]
 
+    # Servo pitch modification logic ~ <ip:port>?p=int
     if "p" in arg_keys and re.match(r"^-?\d+(?:\.\d+)?$", request.args["p"]) is not None:
         ser_app.change_servo(ser_app.pitch, float(request.args["p"]))
         print("Pitch: {}".format(request.args["p"]))
 
+    # Servo yaw modification logic  ~ <ip:port>?y=int
     if "y" in arg_keys and re.match(r"^-?\d+(?:\.\d+)?$", request.args["y"]) is not None:
         ser_app.change_servo(ser_app.yaw, float(request.args["y"]))
         print("Yaw: {}".format(request.args["y"]))
+
+    # Other requests ~ <ip:port>?o=str
+    if "o" in arg_keys:
+        # Face Detection Rotation  ~ <ip:port>?o=f
+        if request.args["o"] == "f":
+            print("FACE DETECT")
 
     # Live video feed
     return Response(generator(Camera()), mimetype="multipart/x-mixed-replace; boundary=frame")
