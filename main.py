b808dd5c006fc9be1bc2810c76cb920abf105824
WiseNat <nathan88wise@gmail.com>
2020-11-07 17:10:00 +0000

    HTTP request structure change

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from servo import ServoController
 web_app = Flask(__name__)
 ser_app = ServoController()
 
+camera = Camera()
+
 
 def generator(cam):
     while True:
@@ -16,21 +18,32 @@ def generator(cam):
                b"Content-Type: image/jpeg\r\n\r\n" + cam.get_frame() + b"\r\n")
 
 
-@web_app.route("/")
-def main_page():
-    # Servo modification logic
+# GET message is send, and the server returns data
+# POST ~ Used to send HTML form data to the server. The data received by the POST method is not cached by the server.
+@web_app.route("/servo")
+def servo():
     arg_keys = [item.lower() for item in list(request.args.keys())]
 
+    # Servo pitch modification logic
     if "p" in arg_keys and re.match(r"^-?\d+(?:\.\d+)?$", request.args["p"]) is not None:
         ser_app.change_servo(ser_app.pitch, float(request.args["p"]))
         print("Pitch: {}".format(request.args["p"]))
 
+    # Servo yaw modification logic
     if "y" in arg_keys and re.match(r"^-?\d+(?:\.\d+)?$", request.args["y"]) is not None:
         ser_app.change_servo(ser_app.yaw, float(request.args["y"]))
         print("Yaw: {}".format(request.args["y"]))
 
-    # Live video feed
-    return Response(generator(Camera()), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+@web_app.route("/photo")
+def photo():
+    # Take photo
+    pass
+
+
+@web_app.route("/")
+def main_page():
+    return Response(generator(camera), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == "__main__":
